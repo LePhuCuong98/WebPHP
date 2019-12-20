f70901e9f4f6c233faf40a6f1c30c57fdb24e50e
unknown <you@example.com>
2019-12-20 23:49:59 +0700

    Add project

---
+++ Views/Backend/HomeView.php
@@ -0,0 +1,5 @@
+<?php 
+	//dat duong dan cho bien $fileLayout de load template vao day
+	$this->fileLayout = "Views/Backend/Layout1.php";
+ ?>
+<h1>Chào mừng bạn đến với các mục quản trị cảu website (Phần mềm mã nguồn mở CNTT)</h1>
---
+++ Views/Frontend/Call.php
@@ -0,0 +1,3 @@
+<?php 
+	echo "Liên hệ với chúng tôi";
+ ?>
